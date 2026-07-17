c6d1ece44c4a0e6ca9efa6b0e14160dbf8160c63
talha-mohsin <muhammadtalhamohsin@gmail.com>
2026-07-18 00:14:32 +0500

    pathn change

--- src/partials/Home/Hero.jsx
+++ src/partials/Home/Hero.jsx
@@ -33,7 +33,7 @@ export default function Hero() {
         </div>
 
         <div className="hero-image-container">
-          <img src="/public/assets/group-books.png" alt="" />
+          <img src="/assets/group-books.png" alt="" />
         </div>
       </div>
     </section>
